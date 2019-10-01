946633db86b77e4311dd9e2298dddba6be63a4ba
xihaoshangdi <xihaoshangdi@163.com>
2019-10-01 17:02:57 +0800

    完成了PC版的画线条和画矩阵

---
+++ Drawingboard/js/main.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  //获取屏幕可视区
+  let width = document.documentElement.clientWidth;
+  let height = document.documentElement.clientHeight;
+  //获取画板
+  let canvas = document.getElementById('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  let ctx = canvas.getContext('2d');
+
+  //色彩和样式
+  ctx.fillStyle = 'black';
+  ctx.strokeStyle = 'black';
+
+  //Drawbox();
+  Drawline();
+  //绘制矩形
+  function Drawbox() {
+    let box = [];
+    canvas.onmousedown = function(e) {
+      let paintting = true;
+      box.push(e.x, e.y);
+      line = [e.x, e.y];
+
+      canvas.onmousemove = function(e) {
+        if (paintting) {
+          ctx.beginPath();
+          ctx.setLineDash([12, 3, 3]);
+          ctx.moveTo(line[0], line[1]);
+          ctx.lineTo(e.x, e.y);
+          line = [e.x, e.y];
+          ctx.stroke();
+        }
+      };
+
+      if (box.length === 4) {
+        width = box[2] - box[0];
+        height = box[3] - box[1];
+        ctx.fillRect(box[0], box[1], width, height);
+        box = [];
+        paintting = false;
+      }
+    };
+  }
+  //绘制线
+  function Drawline() {
+    let paintting = false;
+    let line;
+    canvas.onmousedown = function(e) {
+      paintting = true;
+      line = [e.x, e.y];
+    };
+    canvas.onmousemove = function(e) {
+      if (paintting) {
+        //线条属性
+        ctx.lineWidth = 8;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        //
+        ctx.beginPath();
+        ctx.moveTo(line[0], line[1]);
+        ctx.lineTo(e.x, e.y);
+        line = [e.x, e.y];
+        ctx.stroke();
+      }
+    };
+    canvas.onmouseup = function(e) {
+      paintting = false;
+    };
+  }
+};
